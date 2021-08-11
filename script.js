1709ebfb96293d424ffdd12b9d914943b76cedf4
Sandro <sandrox377@gmail.com>
2021-08-11 18:09:10 -0300

    botão del tarefa

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 let inputarefa = document.getElementById("inputarefa")
 let listaDeTarefas = document.getElementById("listaDeTarefas")
 let botaosalvar = document.getElementById("save")
+let botaoapagar = document.getElementById("del")
 
 
 botaosalvar.addEventListener("click", criartarefa)
 
 function criartarefa(){
   let tarefa = inputarefa.value
-  let li = criarli(tarefa)
+  if (tarefa == ""){
+    alert("Por favor, digite uma tarefa...")
+  
+  }
+  else{
+    
+    let li = criarli(tarefa)
   listaDeTarefas.appendChild(li)
   inputarefa.value = ""
+  storage()
+  }
+  
+  function storage(){
+    let ul = listaDeTarefas.innerHTML
+    localStorage.setItem("lista", ul)
+    
+  }
   
   
- 
-
+  
 
 }
 function criarli(tarefa){
@@ -24,6 +38,7 @@ function criarli(tarefa){
   let newspanbotão = document.createElement("span")
   let newbotão = document.createElement("button")
   newbotão.innerHTML = "x"
+  newbotão.addEventListener("click",delttarefa)
   newbotão.classList.add("botãodelet")
   li.appendChild(newtarefa)
   li.appendChild(newspanbotão)
@@ -33,8 +48,36 @@ function criarli(tarefa){
 }
 
 
+botaoapagar.addEventListener("click",apagar)
+
+function apagar(){
+      localStorage.clear()
+      listaDeTarefas.innerHTML = ""
+       
+
+}
 
 
 
 
+  onload = function (){
+ 
+    let ls = localStorage.getItem("lista")
+    
+    listaDeTarefas.innerHTML = ls
 
+}
+function delttarefa(){
+  let element = this.parentElement
+  let task = element.parentElement
+  listaDeTarefas.removeChild(task)
+  
+
+}
+ 
+
+
+
+
+// <li><span class="span1">hjkljhxzklkl</span>
+// <span><button class="botãodelet">x</button></span></li>
